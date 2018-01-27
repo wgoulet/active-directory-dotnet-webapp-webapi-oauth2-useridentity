50c4f2aac607f5b648493cdd0b01913fab7aaa67
wgoulet <wgoulet@gmail.com>
2018-01-27 01:21:32 -0600

    Updates to migrate to more manual OAuth auth flows for instructional purposes.

---
+++ WebApp/Models/OAuthDataStore.cs
@@ -0,0 +1,36 @@
+namespace WebApp.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class OAuthDataStore : DbContext
+    {
+        // Your context has been configured to use a 'OAuthDataStore' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'WebApp.Models.OAuthDataStore' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'OAuthDataStore' 
+        // connection string in the application configuration file.
+        public OAuthDataStore()
+            : base("name=OAuthDataStore")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+
+        public virtual DbSet<OAuthTokenSet> OAuthTokens { get; set; }
+    }
+
+    public class OAuthTokenSet
+    {
+        public int Id { get; set; }
+        public string accessToken { get; set; }
+        public string bearerToken { get; set; }
+        public string refreshToken { get; set; }
+        public string userId { get; set; }
+
+    }
+}
